7cf1154621f236cf603e1c84a10c309a3bd296d4
sjung <sjung2393@gmail.com>
2017-02-14 14:17:47 +0900

    Add UserLoginAPIView

--- src/accounts/api/serializers.py
+++ src/accounts/api/serializers.py
@@ -2,6 +2,8 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth import get_user_model
 
 from rest_framework.serializers import (
+    CharField,
+    EmailField,
     ModelSerializer,
     HyperlinkedIdentityField,
     SerializerMethodField,
@@ -11,16 +13,50 @@ from rest_framework.serializers import (
 User = get_user_model()
 
 class UserCreateSerializer(ModelSerializer):
+    # Override the default email field in order to allow custom validator,
+    # and create custom email2 field
+    email = EmailField(label="Email address")
+    email2 = EmailField(label="Confirm Email")
     class Meta:
         model = User
         fields = [
         'username',
         'password',
         'email',
+        'email2',
         ]
         extra_kwargs = {'password':
                             {'write_only':True}
                         }
+
+    def validate(self, data):
+    #     email = data['email']
+    #     user_qs = User.objects.filter(email=email)
+    #     if user_qs.exists():
+    #         raise ValidationError('This user already exists')
+        return data
+
+    def validate_email(self, value):
+        initial_data = self.get_initial()
+        email1 = initial_data.get('email2')
+        email2 = value
+
+        #Validation to check whether username is already registered already exists,
+        # but validation doesn't exist for email. Create custom validation
+        user_qs = User.objects.filter(email=email2)
+        if user_qs.exists():
+            raise ValidationError('This user already exists')
+
+        if email1 != email2:
+            raise ValidationError('Emails must match')
+        return value
+    def validate_email2(self, value):
+        initial_data = self.get_initial()
+        email1 = initial_data.get('email')
+        email2 = value
+        if email1 != email2:
+            raise ValidationError('Emails must match')
+        return value
     def create(self, validated_data):
         print(validated_data)
         username = validated_data['username']
@@ -32,4 +68,31 @@ class UserCreateSerializer(ModelSerializer):
                         )
         user_obj.set_password(password)
         user_obj.save()
+
         return validated_data
+
+
+class UserLoginSerializer(ModelSerializer):
+    # Override the default email field in order to allow custom validator,
+    # and create custom email2 field
+    token = CharField(allow_blank=True, read_only=True)
+    username = CharField()
+    email = EmailField(label="Email address")
+    class Meta:
+        model = User
+        fields = [
+        'username',
+        'password',
+        'email',
+        'token',
+        ]
+        extra_kwargs = {'password':
+                            {'write_only':True}
+                        }
+
+    def validate(self, data):
+    #     email = data['email']
+    #     user_qs = User.objects.filter(email=email)
+    #     if user_qs.exists():
+    #         raise ValidationError('This user already exists')
+        return data
--- src/accounts/api/views.py
+++ src/accounts/api/views.py
@@ -1,6 +1,11 @@
 from django.contrib.auth import get_user_model
-
 from django.db.models import Q
+
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+from rest_framework.views import APIView
+
+
 from rest_framework.filters import (
     SearchFilter, OrderingFilter
     )
@@ -28,6 +33,7 @@ from posts.api.permissions import IsOwnerOrReadOnly
 
 from .serializers import (
     UserCreateSerializer,
+    UserLoginSerializer,
     )
 
 User = get_user_model()
@@ -36,4 +42,14 @@ class UserCreateAPIView(CreateAPIView):
     serializer_class = UserCreateSerializer
     queryset = User.objects.all()
 
+class UserLoginAPIView(APIView):
+    permission_classes = [AllowAny]
+    serializer_class = UserLoginSerializer
 
+    def post(self, request, *args, **kwargs):
+        data = request.data #request.POST
+        serializer = UserLoginSerializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            new_data = serializer.data
+            return Response(new_data, status=HTTP_200_OK)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
